73698577e5a447c0b176e2545bf60634133890b2
charcodes24 <charcodes24@gmail.com>
2021-08-16 11:41:08 -0700

    commit tree 50ae272f48930ffc0d5661a448c59b1d32d87e3a

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    # students = Student.all
+    # render json: students
+    students = if params[:name]
+      Student.find_by(first_name: params[:name]) || Student.find_by(last_name: params[:name])
+    else
+      Student.all
+    end
+    render json: students 
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student 
   end
 
 end
